4cd9e2e4bc4481e11ebd8c456c9ea44ed93ae599
Ricky Theil <rtheil@codirt.com>
2020-08-27 23:34:26 -0600

    added options

--- src/components/formElements/submitButton.jsx
+++ src/components/formElements/submitButton.jsx
@@ -3,15 +3,26 @@ import { Button, Spinner } from "react-bootstrap";
 
 class SubmitButton extends Component {
   render() {
-    let { text, loadingText, loading, variant } = this.props;
-    if (loadingText === undefined) loadingText = "Loading...";
-    if (variant === undefined) variant = "primary";
+    let {
+      text,
+      loadingText = "Loading...",
+      loading = false,
+      variant = "primary",
+      onClick,
+      name,
+    } = this.props;
 
     //add a space before loading text
-    loadingText = <React.Fragment>&nbsp;{loadingText}</React.Fragment>;
+    loadingText = <>&nbsp;{loadingText}</>;
 
     return (
-      <Button variant={variant} type="submit" disabled={loading}>
+      <Button
+        variant={variant}
+        type="submit"
+        disabled={loading}
+        onClick={onClick}
+        name={name}
+      >
         {loading && (
           <Spinner
             as="span"
