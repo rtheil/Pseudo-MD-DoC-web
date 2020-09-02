10010b2af5a5a2854b25d5c5648ceee7e15e2230
Ricky Theil <rtheil@codirt.com>
2020-09-01 21:53:26 -0600

    rename yesNoInput to selectInput

--- src/components/formElements/SelectInput.jsx
+++ src/components/formElements/SelectInput.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Form, Col } from "react-bootstrap";
 
-export default function YesNoSelect({ name, label, value, size, onChange }) {
+export default function SelectInput({
+  name,
+  label,
+  value,
+  onChange,
+  options = [
+    { value: false, text: "No" },
+    { value: true, text: "Yes" },
+  ],
+}) {
   return (
     <Form.Group as={Col}>
       <Form.Label>{label}</Form.Label>
@@ -15,8 +24,11 @@ export default function YesNoSelect({ name, label, value, size, onChange }) {
         onChange={onChange}
         value={value}
       >
-        <option value={false}>No</option>
-        <option value={true}>Yes</option>
+        {options.map((option, i) => (
+          <option value={option.value} key={i}>
+            {option.text}
+          </option>
+        ))}
       </Form.Control>
     </Form.Group>
   );
